ead912633a61616d09fda50522ef8d4c74c1fc63
o-rchid <ozzve35@gmail.com>
2025-03-24 21:40:39 +0900

    first commit

---
+++ src/main/java/com/monday/high/MondayBatchApplication.java
@@ -0,0 +1,13 @@
+package com.monday.high;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MondayBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MondayBatchApplication.class, args);
+	}
+
+}
