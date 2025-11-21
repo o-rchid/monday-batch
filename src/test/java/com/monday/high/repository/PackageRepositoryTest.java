234110a57fa9d45568450d1d9db1fb138459e4c1
정혜 최 <ozzve35@gmail.com>
2025-11-21 17:59:22 +0900

    temp

--- src/test/java/com/monday/high/repository/PackageRepositoryTest.java
+++ src/test/java/com/monday/high/repository/PackageRepositoryTest.java
@@ -7,8 +7,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @Slf4j
@@ -30,5 +36,45 @@ public class PackageRepositoryTest {
 
         // Then
         assertNotNull(packageEntity.getPackageSeq());
+    }
+
+    @Test
+    public void test_findByCreatedAfter() {
+
+        // given
+        LocalDateTime dateTime = LocalDateTime.now().minusMinutes(1);
+
+        PackageEntity packageEntity = new PackageEntity();
+        packageEntity.setPackageName("학생 전용 3개월");
+        packageEntity.setPeriod(90);
+        packageRepository.save(packageEntity);
+
+        PackageEntity packageEntity1 = new PackageEntity();
+        packageEntity.setPackageName("학생 전용 6개월");
+        packageEntity.setPeriod(180);
+        packageRepository.save(packageEntity1);
+
+        // when
+        final List<PackageEntity> packageEntities = packageRepository.findByCreatedAtAfter(dateTime, PageRequest.of(0, 1, Sort.by("packageSeq").descending()));
+
+        // then
+        assertEquals(1, packageEntities.size());
+        assertEquals(packageEntity.getPackageSeq(), packageEntities.get(0).getPackageSeq());
+    }
+
+    @Test
+    public void test_updateCountAndPeriod() {
+
+        // given
+        PackageEntity packageEntity = new PackageEntity();
+        packageEntity.setPackageName("바디프로필 이벤트 4개월");
+        packageEntity.setPeriod(90);
+        packageRepository.save(packageEntity);
+
+        // when
+        final PackageEntity updatedPackageEntity = packageEntity.findById(packageEntity.getPackageSeq()).get();
+
+        // then
+        assertEquals(30, updatedPackageEntity.getCount());
     }
 }
